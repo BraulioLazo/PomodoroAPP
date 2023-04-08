53aab9b3587c1be8d97a6ce3c1b327477bf9a5a5
Braulio Lazo <yobraulio23@gmail.com>
2023-04-08 13:00:56 -0400

    Posicionamiento correcto de los NUMEROS

--- JavaScript/API-wake-Lock.js
+++ JavaScript/API-wake-Lock.js
@@ -7,46 +7,37 @@
 */
 
 
-/* 
-   Es importante verificar la compatibilidad de esta API
-   con el navegador antes de hacer la peticion
-*/
-function compatibilityWakeLock() {
-    if ("wakeLock" in navigator) {
-        requestScreenLock();
-    } else {
-        console.log("Su navegador no soporta el API Wake Lock");
-    }
-}
+const wakeLockAPI = {
+    wakeLockObj: null,
 
-/* En esta función hacemos la petición de bloqueo a la API*/
-let wakeLock = null;
-async function requestScreenLock() {
-    try {
-        wakeLock = await navigator.wakeLock.request();
-        console.log("La pantalla ha sido bloqueda correctamente");
-        console.log(wakeLock);
-    } catch (error) {
-        console.error("Error al bloquear pantalla: " + error);
-    }
-}
-
-/* 
-   En esta función liberamos el bloqueo de la pantalla. Es importante no abusar 
-   del uso del bloqueo ya que esto representa un mayor gasto en la batería del dispositivo.
-*/
-async function disableScreenLock() {
-    try {
-        if (wakeLock === null) {
-            console.log("La pantalla ya esta desbloqueda");
-            return;
+    checkCompatibility: () => {
+        if ("wakeLock" in navigator) {
+            wakeLockAPI.requestScreenLock();
+        } else {
+            console.log("Navegador no compatible con el API Wake Lock");
         }
-        await wakeLock.release();
-        console.log("Pantalla Desbloqueada");
-        console.log(wakeLock);
-        wakeLock = null;
+    },
 
-    } catch (error) {
-        console.error("Error al Desbloquear pantalla: " + error);
+    requestScreenLock: async () => {
+        try {
+            wakeLockAPI.wakeLockObj = await navigator.wakeLock.request('screen');
+            console.log("La pantalla ha sido BLOQUEADA: ");
+            console.log(wakeLockAPI.wakeLockObj);
+        } catch (error) {
+            console.error("Error al bloquear: " + error);
+        }
+    },
+
+    unlockScreen: async () => {
+        try {
+            await wakeLockAPI.wakeLockObj.release();
+            console.log("Pantalla desbloqueda");
+            console.log(wakeLockAPI.wakeLockObj);
+            wakeLockAPI.wakeLockObj = null;
+        } catch (error) {
+            console.error("Error al desbloquear la pantalla: " + error);
+        }
     }
-}
+
+};
+
